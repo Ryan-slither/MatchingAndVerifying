1863b1350e2af75f6aa4a2a93228a4eff1725221
yoanexposito <yoan.exposito@ufl.edu>
2026-01-27 03:18:14 -0500

    created simple logic to handle matcher and verifier output in main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import sys
 
 from io_lib.parser import Parser
 from matcher_b import generateMatchings
+from verifier_lib.verifier import match_verifier, stability_verifier
+
 
 if __name__ == "__main__":
     arg_count = len(sys.argv)
@@ -12,4 +14,27 @@ if __name__ == "__main__":
     p = Parser()
     p.parse_input_file(input_file)
 
-    print(generateMatchings(p.n, p.preferences_a, p.preferences_b))
+    n = p.n
+    hospital_prefs = {i + 1: p.preferences_a[i] for i in range(n)}
+    student_prefs = {i + 1: p.preferences_b[i] for i in range(n)}
+
+    matching = generateMatchings(n, hospital_prefs, student_prefs)
+
+    final_matching = {
+        h + 1: (matching[h] + 1 if matching[h] != -1 else -1)
+        for h in range(n)
+    }
+
+
+    for h in range(1, n + 1):
+        print(h, final_matching[h])
+
+    if not match_verifier(final_matching, n):
+        sys.exit(1)
+
+    if stability_verifier(final_matching, hospital_prefs, student_prefs, n):
+        print("VALID STABLE")
+    else:
+        print("UNSTABLE")
+
+
